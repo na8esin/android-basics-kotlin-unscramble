517719d9ba0b29b3e06c7e491f5efc67cd2ea995
sikmi_watanabe <na8esin.mpst@gmail.com>
2023-10-23 16:32:11 +0900

    getNextWord()実装

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,12 +6,34 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
     private var score = 0
     private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            // ここで再帰呼び出しの結果、初期化されるからlateinitだけどわかりづらくないのか？
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
     init {
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
 
     override fun onCleared() {
